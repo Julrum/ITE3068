f8f6df8c2e23e852e378d77b2b9ce0b04f243fde
Junwon Park <julrumg@gmail.com>
2021-09-16 12:02:37 +0900

    Add Timer

---
+++ timer/src/Timer.js
@@ -0,0 +1,12 @@
+let Timer = ({ remained }) => {
+  return (
+    <div>
+      <h1>
+        {Math.floor(remained / 60)} :{" "}
+        {remained % 60 < 10 ? "0" + (remained % 60) : remained % 60}
+      </h1>
+    </div>
+  );
+};
+
+export default Timer;
