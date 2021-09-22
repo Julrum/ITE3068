7540fe194f3150328c881a9eb2d2dc6efa8f9b80
Junwon Park <julrumg@gmail.com>
2021-09-22 16:59:40 +0900

    Initial commit

--- timer/src/App.js
+++ timer/src/App.js
@@ -1,11 +1,22 @@
-import "./App.css";
-import TimerPannel from "./TimerPannel";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <TimerPannel />
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
